bbaa289466083e06854acf4b1bfb0b4c1e185508
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-31 18:47:35 +0000

    Split name field into first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   
-  validates :name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   
   validates :email, presence: true, uniqueness: { 
     case_sensitive: false,
