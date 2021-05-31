fedb5b9ba63a462deb35b788d7fcc42cc161b570
Lily Habberfield <lilyhabbs@gmail.com>
2021-05-31 16:13:37 +0000

    Validate minimum password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ActiveRecord::Base
-
+  
   validates :name, presence: true
-
+  
   validates :email, presence: true, uniqueness: { 
     case_sensitive: false,
     message: ->(user, data) do
       "#{data[:value]} is already taken."
-  end
+    end
   }
-
+  
+  validates :password, length: { minimum: 8 }
+  
   has_secure_password
   
 end
